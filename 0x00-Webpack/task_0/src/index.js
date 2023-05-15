1d83c565c2a9fd7862cd90f91ed7495422ae93e9
scaarifintl <scaarifintl@gmail.com>
2023-05-15 17:56:51 +0300

    t-0 setup webpack, the basic/simple case

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+$(document).ready(function () {
+  const paragraphs = '<p>Holberton Dashboard</p><p>Dashboard data for the students</p><p>Copyright - Holberton School</p>';
+  $('body').append(paragraphs);
+});
