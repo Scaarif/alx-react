05fb80e79e279e277804c5ee194dffa27d5fc67b
scaarifintl <scaarifintl@gmail.com>
2023-05-17 09:26:19 +0300

    add login form

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -9,9 +9,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <div id="root-notifications">
-      <Notifications />
-    </div>
+  </React.StrictMode>
+);
+
+const notifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
+notifications.render(
+  <React.StrictMode>
+     <Notifications />
   </React.StrictMode>
 );
 
