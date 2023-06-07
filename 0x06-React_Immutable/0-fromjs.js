315a43d4f99e0e25aa8089dd803998c0653a6388
scaarifintl <scaarifintl@gmail.com>
2023-06-07 12:27:47 +0300

    name parameter object...

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,6 @@
 import { fromJS } from 'immutable';
 
-export default function getImmutableObject (obj) {
-  return fromJS(obj);
+export default function getImmutableObject (object) {
+  // fromJS deeply converts plain JS objs and arrays to Immutable Maps and Lists
+  return fromJS(object);
 }
