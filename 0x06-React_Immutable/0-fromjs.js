8e1bb0918c3cc8c4c23675dada7e0d7b01a5e2c5
scaarifintl <scaarifintl@gmail.com>
2023-06-07 12:12:04 +0300

    export default instead

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,7 @@
 /* defines a function accepts an `object` as a parameter and converts it inyto an immutable Map using fromJS of Immutable.js */
 
-const { fromJS } = require('immutable')
-export function getImmutableObject(obj) {
-	// fromJS deeply converts plain JS objs and arrays to Immutable Maps and Lists
-	return fromJS(obj)
+const { fromJS } = require('immutable');
+export default function getImmutableObject (obj) {
+  // fromJS deeply converts plain JS objs and arrays to Immutable Maps and Lists
+  return fromJS(obj);
 }
--- 0x06-React_Immutable/0-main.js
+++ 0x06-React_Immutable/0-main.js
@@ -1,8 +1,8 @@
-import { getImmutableObject } from './0-fromjs.js'
+import getImmutableObject  from './0-fromjs.js';
 const obj = {
 	     fear: true,
 	     smell: -1033575916.9145899,
 	     wall: false,
 	     thing: -914767132
-	}
-console.log(getImmutableObject(obj))
+};
+console.log(getImmutableObject(obj));
