94cccc3bd336e2a5fa602a81386e3723316cec07
scaarifintl <scaarifintl@gmail.com>
2023-06-07 12:19:28 +0300

    import instead of requiring fromJS

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,6 +1,6 @@
-/* defines a function accepts an `object` as a parameter and converts it inyto an immutable Map using fromJS of Immutable.js */
+/* defines a function accepts an `object` as a parameter and converts it into an immutable Map using fromJS of Immutable.js */
+import { fromJS } from 'immutable'
 
-const { fromJS } = require('immutable');
 export default function getImmutableObject (obj) {
   // fromJS deeply converts plain JS objs and arrays to Immutable Maps and Lists
   return fromJS(obj);
