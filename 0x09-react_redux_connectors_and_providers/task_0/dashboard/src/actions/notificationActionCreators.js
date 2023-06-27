9a043609b368b8f0cb6fa183acd9302f25ddf8bc
scaarifintl <scaarifintl@gmail.com>
2023-06-27 08:06:02 +0300

    initialize project

---
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,22 @@
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
+
+export const markAsRead = (index) => {
+    return {
+        type: MARK_AS_READ,
+        index // i.e. index: index
+    }
+}
+
+export const setNotificationFilter = (filter) => {
+    return {
+        type: SET_TYPE_FILTER,
+        filter
+    }
+}
+
+export const fetchNotifications = (data) => {
+    return {
+        type: FETCH_NOTIFICATIONS_SUCCESS,
+        data // data: data
+    }
+}
